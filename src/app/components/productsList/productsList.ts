94c4c24583ce1590035d1b7cbb836125be3cbcf9
Sarkhan <abdullayevsarxan92@gmail.com>
2022-07-14 13:45:53 +0400

    feat: adding buttons

--- src/app/components/productsList/productsList.ts
+++ src/app/components/productsList/productsList.ts
@@ -1,6 +1,23 @@
+import { ProductItem } from "../productItem"
+
 export class productsList {
+    private products: ProductItem[] = [
+        new ProductItem(),
+        new ProductItem(),
+        new ProductItem(),
+        new ProductItem(),
+    ]
+
+
     render() {
-        return `<h2>Products list</h2>`
+        return `<h2>Products list</h2>
+        ${this.products.map((product) => product.render()).join('') }
+        <p>----</p>
+        <div>
+        <button>prev</button>
+        <button>next</button>
+        </div>
+        `
     }
 }
 
