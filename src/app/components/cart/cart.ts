85f56738bcf66fe3d740dbee3cdf4e0d0c3ca6b8
Sarkhan <abdullayevsarxan92@gmail.com>
2022-07-17 23:00:01 +0400

    feat: adding cart function

--- src/app/components/cart/cart.ts
+++ src/app/components/cart/cart.ts
@@ -1,4 +1,21 @@
+import { CartProducts } from "../../store/state";
+import { store } from "../../store/store";
+
 export class Cart {
+
+    private products: CartProducts = {};
+    private amount: number = 0;
+    private sum: number = 0;
+
+
+    constructor() {
+      store.$state.subscribe(({cart}) => {
+        this.products = cart.products;
+        this.amount = Object.keys(this.products).length;
+        this.sum = Object.values(this.products).reduce((sum, item) => sum + item.product.price, 0);
+      });
+    }
+
     render() {
         return `
 
@@ -6,30 +23,21 @@ export class Cart {
 
       <p style="color: white; background-color: blue; text-shadow: 1px 1px; font-size: 24px;";>Cart</p>
       <ul class="list-group" style="margin-top: 1em;">
-            <li class="list-group-item d-flex justify-content-between align-items-center">
-            An item ($120)
-            <a href="#" class="btn">➕</a>
-            <a href="#" class="btn">➖</a>
-            <span class="badge bg-primary rounded-pill">14</span>
-            </li>
+          ${Object.values(this.products).map(({amount, product}) => `
+          <li class="list-group-item d-flex justify-content-between align-items-center">
+          ${product.name} ($${product.price})
+          <a href="#" class="btn">➕</a>
+          <a href="#" class="btn">➖</a>
+          <span class="badge bg-primary rounded-pill">${amount}</span>
+          </li>
+          
+          `).join('')}
 
-            <li class="list-group-item d-flex justify-content-between align-items-center">
-            An item ($120)
-            <a href="#" class="btn">➕</a>
-            <a href="#" class="btn">➖</a>
-            <span class="badge bg-primary rounded-pill">14</span>
-            </li>
 
-            <li class="list-group-item d-flex justify-content-between align-items-center">
-            An item ($120)
-            <a href="#" class="btn">➕</a>
-            <a href="#" class="btn">➖</a>
-            <span class="badge bg-primary rounded-pill">14</span>
-            </li>
        </ul>
       
         <p>
-            Summary: 12 products, $1289
+            Summary: ${this.amount} products, $${this.sum}
         </p>
 
       </div>
